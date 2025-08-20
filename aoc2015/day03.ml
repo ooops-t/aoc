1995e207ced507c0bb306d22313560e0097eeccc
Ooops_t <ooops_t@163.com>
2025-08-20 16:07:16 +0800

    aoc2015 day03 puzzle 02
    Signed-off-by: Ooops_t <ooops_t@163.com>

--- aoc2015/day03.ml
+++ aoc2015/day03.ml
@@ -65,6 +65,44 @@ let puzzle_01' =
          PointSet.cardinal set)
   |> List.fold_left ( + ) 0
 
+let rec move_two x y x1 y1 set idx chars =
+  match chars with
+  | [] -> PointSet.cardinal set
+  | h :: t ->
+      if idx mod 2 = 0 then
+        let x, y =
+          match h with
+          | '^' -> (x - 1, y)
+          | '>' -> (x, y + 1)
+          | 'v' -> (x + 1, y)
+          | '<' -> (x, y - 1)
+          | _ -> (x, y)
+        in
+        let set = PointSet.add (Printf.sprintf "%d,%d" x y) set in
+        move_two x y x1 y1 set (idx + 1) t
+      else
+        let x1, y1 =
+          match h with
+          | '^' -> (x1 - 1, y1)
+          | '>' -> (x1, y1 + 1)
+          | 'v' -> (x1 + 1, y1)
+          | '<' -> (x1, y1 - 1)
+          | _ -> (x1, y1)
+        in
+        let set = PointSet.add (Printf.sprintf "%d,%d" x1 y1) set in
+        move_two x y x1 y1 set (idx + 1) t
+
+let puzzle_02 =
+  let set = PointSet.singleton "0,0" in
+  List.map
+    (fun line -> line |> String.to_seq |> List.of_seq |> move_two 0 0 0 0 set 1)
+    lines
+  |> List.fold_left ( + ) 0
+
 let () =
-  Printf.printf "Day 03:\npuzzle_01 answer: %d\npuzzle_01' answer: %d\n"
-    puzzle_01 puzzle_01'
+  Printf.printf
+    "Day 03:\n\
+     puzzle_01 answer: %d\n\
+     puzzle_01' answer: %d\n\
+     puzzle_02 answer: %d\n"
+    puzzle_01 puzzle_01' puzzle_02
